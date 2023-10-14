bb7c0472648f964479f901a0bfa065cd64d29e21
saleh <saleh@DESKTOP-GPNKQRG>
2023-10-14 04:51:27 +0300

    changed contact and linkedlist and phonebook

---
+++ myproj/src/linkedlist.java
@@ -0,0 +1,120 @@
+public class linkedlist <T> implements list <T>{
+public Node<T> head;
+public Node<T> current;
+public linkedlist() {
+	head=current=null;
+}
+@Override
+public void findfirst() {
+	current=head;
+	
+}
+@Override
+public void findnext() {
+	current=current.next;
+	
+}
+@Override
+public T retreive() {
+	
+	return current.data;
+}
+@Override
+public void update(T e) {
+	current.data=e;
+	
+}
+@Override
+public void insert(T e) {
+	Node<T> tmp;
+	if(search(e)) {
+		System.out.println("the contact is there");
+		return ;}
+	if(head==null)
+		head=current=new Node<T>(e);
+	else {
+		tmp=current.next;
+		current.next=new Node<T>(e);
+		current=current.next;
+		current.next=tmp;
+		
+	}
+	
+}
+@Override
+public void remove() {
+	Node<T> tmp=head;
+	if(head==current) {
+		head=head.next;
+	}else {
+		while(tmp.next!=current) {
+			tmp=tmp.next;
+		}
+		tmp.next=current.next;
+	}
+	if(current.next==null)
+		current=head;
+	else
+		current=current.next;
+}
+@Override
+public boolean full() {
+	return false;
+}
+@Override
+public boolean last() {
+	return current.next==null;
+}
+@Override
+public boolean empty() {
+	return head==null;
+}
+public Contact searcbyname(T e) { // changed searchbyname to return Contact
+	Node<T> tmp=head;
+	Node<T>Searchdata= new Node<T>(e);
+	while(tmp!=null) {
+		if(((Contact)tmp.data).getContactName()==((Contact)Searchdata.data).getContactName()) 
+			return (Contact) tmp.data;
+		tmp=tmp.next;}
+	return null;
+}
+public boolean searchbyeamil(T e) {
+	Node<T> tmp=head;
+	Node<T>Searchdata= new Node<T>(e);
+	while(tmp!=null) {
+		if(((Contact)tmp.data).getEmailAddress()==((Contact)Searchdata.data).getEmailAddress()) 
+			return true;
+		tmp=tmp.next;}
+	return false;
+}
+public boolean searchbyAdress(T e) {
+	Node<T> tmp=head;
+	Node<T>Searchdata= new Node<T>(e);
+	while(tmp!=null) {
+		if(((Contact)tmp.data).getAddress()==((Contact)Searchdata.data).getAddress()) 
+			return true;
+		tmp=tmp.next;}
+	return false;
+}
+public boolean searchbyBirthday(T e) {
+	Node<T> tmp=head;
+	Node<T>Searchdata= new Node<T>(e);
+	while(tmp!=null) {
+		if(((Contact)tmp.data).getBirthday()==((Contact)Searchdata.data).getBirthday()) 
+			return true;
+		tmp=tmp.next;}
+	return false;
+}
+public boolean searchbyphone(T e) {
+	Node<T> tmp=head;
+	Node<T>Searchdata= new Node<T>(e);
+	while(tmp!=null) {
+		if(((Contact)tmp.data).getPhoneNumber()==((Contact)Searchdata.data).getPhoneNumber()) 
+			return true;
+		tmp=tmp.next;}
+	return false;
+}
+
+
+
+}
---
+++ myproj/src/phonebook.java
@@ -0,0 +1,116 @@
+
+import java.util.*;
+public class phonebook<T> extends linkedlist<T> {
+private linkedlist<Contact> LinkListConatact; 
+private linkedlist<Event> LinkListEvent;
+public Scanner input=new Scanner(System.in); //check
+public phonebook() {}
+
+public void addcontact() {
+	 
+	System.out.println("enter the contact name");
+	System.out.println("enter the contact phone number");
+	System.out.println("enter the contact email address");
+	System.out.println("enter the contact addresss");
+	System.out.println("enter the contact birthday");
+	System.out.println("enter the contact notes for contact");
+Contact c=new Contact(input.next(),input.nextInt(),input.next(),input.next(),input.nextInt(),input.next());
+LinkListConatact.insert(c);
+	
+}
+
+public void searchcontact() {
+	int select; // new integer for switch
+	do {
+	System.out.println("Enter search criteria:\r\n"
+			+ "1. Name\r\n"
+			+ "2. Phone Number\r\n"
+			+ "3. Email Address\r\n"
+			+ "4. Address\r\n"
+			+ "5. Birthday");
+	
+	select = input.nextInt();
+	switch(select) {
+		case 1: System.out.println("enter conatc's name");    //finished case 1 for name
+			T currentname = (T) input.next();
+			Contact cont = searcbyname(currentname);
+			if(cont != null) {
+				System.out.println("Contact found!");
+				System.out.println(cont.toString());
+			}
+			else
+				System.out.print("cant find contact");
+			break;
+		case 2: 
+			break;
+		case 3:
+			break;
+		case 4:
+			break;
+		case 5:
+			break;
+		default:
+			System.out.println("Sorry wrong number");
+		}
+	} while(select < 1 || select > 5); // when user inputs wrong number
+}
+
+
+public void Mneu() {
+	int select=0;
+	do {
+		System.out.println("Welcome to the Linked Tree Phonebook!\r\n"
+				+ "Please choose an option:\r\n"
+				+ "1. Add a contact\r\n"
+				+ "2. Search for a contact\r\n"
+				+ "3. Delete a contact\r\n"
+				+ "4. Schedule an event\r\n"
+				+ "5. Print event details\r\n"
+				+ "6. Print contacts by first name\r\n"
+				+ "7. Print all events alphabetically\r\n"
+				+ "8. Exit\r\n");
+		 
+		select=input.nextInt();
+		switch(select) {
+		case 1: addcontact();
+			
+			
+			break;
+		case 2: 
+			break;
+		case 3:
+			break;
+		case 4:
+			break;
+		case 5:
+			break;
+		case 6:
+			break;
+		case 7:
+			break;
+	}}while(select!=8);
+	System.out.println("goodbye!");
+	}
+	
+
+
+
+@Override
+public void insert(T e) {
+	// TODO Auto-generated method stub
+	super.insert(e);
+}
+public void printallcontact() {
+	current=head;
+	while(current!=null) {
+		System.out.print(((Contact)current.data).getContactName());
+		System.out.print(((Contact)current.data).getPhoneNumber());
+		System.out.print(((Contact)current.data).getEmailAddress());
+		System.out.print(((Contact)current.data).getAddress());
+		System.out.print(((Contact)current.data).getBirthday());
+		System.out.println(((Contact)current.data).getNote());
+
+		current=current.next;
+	}
+}
+}
